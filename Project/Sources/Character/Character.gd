cdc6f0d8456c9be45e6fdc4013cab768ac065039
Reituag <gauthierhobbes@free.fr>
2021-05-09 18:24:17 +0200

    First working version
    Moving character, basic behaviour

---
+++ Project/Sources/Character/Character.gd
@@ -0,0 +1,44 @@
+extends Area2D
+
+export var speed = 400  # How fast the character will move (pixels/sec).
+var screen_size  # Size of the game window.
+
+signal hit
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	hide()
+	screen_size = get_viewport_rect().size
+	connect("body_entered",self, "_on_Character_body_entered")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2()  # The player's movement vector.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play("walk")
+		$AnimatedSprite.flip_h = velocity.x < 0
+	else:
+		$AnimatedSprite.play("idle")
+	
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+func _on_Character_body_entered(body):
+#    hide()  # Player disappears after being hit.
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
