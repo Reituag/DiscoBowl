442390149788b106dbb544807a4ab0a93622c828
Reituag <gauthierhobbes@free.fr>
2021-07-01 18:57:57 +0200

    Popup addition when hitting localgame

--- Project/Sources/UI/Menus/MainMenu.gd
+++ Project/Sources/UI/Menus/MainMenu.gd
@@ -1,20 +1,38 @@
 extends Control
 
+var CharSelectMenu = preload("res://Sources/UI/CharacterSelection/CharSelect_Menu.tscn")
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# Memorization of the popup menu use to configurate the local game
+onready var localPopup = $PopupLocal
+onready var nbPlayerTxt = $PopupLocal/VBoxContainer/LineEdit
+onready var nbplayerSlider = $PopupLocal/VBoxContainer/HSlider
 
-
-# Called when the node enters the scene tree for the first time.
+## Scene configuration ##
 func _ready():
-	pass # Replace with function body.
+	localPopup.popup_exclusive = true
 
+## Specific methods ##
+func _create_local_game():
+	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
+## Event handling ##
 func _on_ButtonLocal_pressed():
-	$PopupLocal.popup()
+	localPopup.popup()
+
+func _on_ButtonCancel_pressed():
+	localPopup.hide()
+
+func _on_LineEdit_text_changed(new_text):
+	var ok_str = "1_2_3_4_5_6_7_8"
+	# Ensures that the number of players is an integer in [1;8]
+	if (new_text in ok_str):
+		nbPlayerTxt.text = new_text
+		nbplayerSlider.value = float(new_text)
+	else:
+		nbPlayerTxt.text = "2"
+		nbplayerSlider.value = 2.0
+
+
+
+func _on_HSlider_value_changed(value):
+	nbPlayerTxt.text = String(value)
