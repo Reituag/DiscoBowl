384836f43eb07ca04821ef7aaa165c7f3fd92883
Reituag <gauthierhobbes@free.fr>
2021-07-14 00:10:37 +0200

    Game configuration addition
    Data coming from the controller selection menu is now used to configurate the local game.
    Default data are provided in the localgame scene

--- Project/Sources/Game/LocalGame.gd
+++ Project/Sources/Game/LocalGame.gd
@@ -18,34 +18,48 @@ var scene_parameters : Dictionary = {
 					'character' : null} ]
 }
 
+onready var arena : Map = $Arena
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#############
-	# TEST CODE #
-	#############
-	for i in range(5):
-		var player_data = { 'id'        : 'Gurlu',
-							'controller': 'none',
-							'character' : null}
-		player_data['id'] = "player{n}".format({'n':i})
-		if i != 1:
-			var auto = AutoController.new()
-			auto.config("shield")
-			$Characters.get_child(i).start($Arena.startPoints[i], auto)
-			player_data['controller'] = auto
-		else :
-			var kbCtrler = KeyboardCharacterController.new()
-			kbCtrler.config() 
-			$Characters.get_child(i).start($Arena.startPoints[i], kbCtrler)
-			player_data['controller'] = kbCtrler
-		
-		if scene_parameters.size() > i:
-			scene_parameters["players"][i] = player_data
+	
+	var players = scene_parameters['players']
+	# Characters & Controllers instanciation
+	for index in range(players.size()):
+		if index in range(arena.startPoints.size()):
+			# Character instanciation & addition to childs
+			# if the ressource is given in parameters, various characters 
+			# can be loaded
+			var character = \
+				load("res://Sources/Character/Character.tscn").instance()
+			add_child(character)
+			
+			# Controller instanciation & configuration
+			var controller
+			if 'auto' in players[index]['controller']:
+				# Auto Controller configuration
+				controller = AutoController.new()
+				controller.config(players[index]['controller'])
+			elif 'k' in players[index]['controller']:
+				# Keyboard controller configuration
+				controller = KeyboardCharacterController.new()
+				controller.config()
+			elif 'j' in players[index]['controller']:
+				# Joypad controller configuration.
+				# Device index is given in the controller indicator : 
+				# j3 is joypad controller, device number 3
+				controller = JoypadCharacterController.new()
+				controller.config(int(players[index]['controller'].substr(1)))
+			else:
+				# Default controller is an autocontroller with shielding
+				controller = AutoController.new()
+				controller.config('shield')
+			# Start character
+			character.start(arena.startPoints[index], controller)
 		else:
-			scene_parameters['players'].append(player_data)
-		scene_parameters["players"][i]["character"] = $Characters.get_child(i)
-	#############
-
+			# Error information
+			print("Only {2} start points: n°{0}/{1} not added".format( 
+				[index, players.size(), arena.startPoints.size()]))
 
 func _process(_delta):
 	if Input.is_action_pressed("ui_cancel"):
